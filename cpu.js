50d2ef49a2b5780fd1348d21207561168ea57205
HarshVardhan <hrshvrdhn2818@gmail.com>
2021-07-30 16:42:40 +0530

    added some comments to the js file

--- cpu.js
+++ cpu.js
@@ -5,19 +5,20 @@ var cpuHeading = document.getElementsByClassName("cpuheading");
 var logo = document.getElementsByClassName("logo");
 var productsContainer = document.getElementsByClassName("productsContainer");
 
-
+// Noticing the scroll and creating animations accordingly
 $(window).scroll(function scrollAnimation() {
     var scrolledUpLength =  $(window).scrollTop();
-    console.log(scrolledUpLength);
 
+    // Until scrolled page length is less than 500px and greater than 0px.
     if (scrolledUpLength >= 0 && scrolledUpLength < 500) {
         var rollCurtainTo = -2.5*(scrolledUpLength);
+
+        // This reveals the products page.
         $(upperCurtain).css("top", String(rollCurtainTo));
         $(lowerCurtain).css("bottom", String(rollCurtainTo));
-
-        $(productsContainer).css("margin-top", String(2*scrolledUpLength));
-        $(logoNameContainer).css("top", String(300-scrolledUpLength));
-
+        
+        // This moves down the products Container in which products are listed on lower curtain.
+        $(productsContainer).css("margin-top", String(3*scrolledUpLength));
     }    
 });
 
